cadda7ae163cfec510b83dab5df1e36773ec06d7
Taisuke Miyazaki <MiyazakiTaisuke@lifull.com>
2018-02-01 14:17:00 +0900

    bootstrapを導入

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import BootstrapVue from 'bootstrap-vue'
+
 Vue.config.productionTip = false
+Vue.use(BootstrapVue);
 
 /* eslint-disable no-new */
 new Vue({
